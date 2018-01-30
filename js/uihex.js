f0d79897f75e33be91778a2c5e2d70e27909df37
Eugene Belinski <ebelinski@protonmail.com>
2018-01-30 13:38:09 -0600

    refactored code with validateValue

--- js/uihex.js
+++ js/uihex.js
@@ -10,40 +10,53 @@ $(document).ready(function() {
   new Clipboard('.btn');
 });
 
-function processValue(rawValue) {
+function processValue(value) {
   var newValue = "";
 
-  if (((rawValue.length == 7 || rawValue.length == 4) && rawValue[0] == '#') || ((rawValue.length == 6 || rawValue.length == 3) && rawValue[0] != '#')) {
-    if (rawValue[0] == '#') {
-      newValue = rawValue.substring(1);
-    } else {
-      newValue = rawValue;
-    }
-
-    newValue = newValue.toLowerCase();
-
-    if (newValue.length == 3) {
-      newValue = newValue[0]+newValue[0]+newValue[1]+newValue[1]+newValue[2]+newValue[2];
-    }
-
-    var red = getColorValue(newValue[0]+newValue[1]);
-    var grn = getColorValue(newValue[2]+newValue[3]);
-    var blu = getColorValue(newValue[4]+newValue[5]);
-
-    if (isNaN(red) || isNaN(grn) || isNaN(blu)) {
-      clearResults();
-    } else {
-      $("#results").show();
-
-      $("#swift-code").html("UIColor(red: "+red+", green: "+grn+", blue: "+blu+", alpha: 1) // #"+newValue);
-      $("#objective-c-code").html("[UIColor colorWithRed:"+red+" green:"+grn+" blue:"+blu+" alpha:1]; // #"+newValue);
-
-      $(".color-preview.active").css("border-color", "black");
-      $(".color-preview.active").css("background-color", "#"+newValue);
-    }
-  } else {
+  if (!validateValue(value)) {
     clearResults();
+    return;
   }
+   
+  if (value[0] == '#') {
+    newValue = value.substring(1);
+  } else {
+    newValue = value;
+  }
+
+  newValue = newValue.toLowerCase();
+
+  if (newValue.length == 3) {
+    newValue = newValue[0]+newValue[0]+newValue[1]+newValue[1]+newValue[2]+newValue[2];
+  }
+
+  var red = getColorValue(newValue[0]+newValue[1]);
+  var grn = getColorValue(newValue[2]+newValue[3]);
+  var blu = getColorValue(newValue[4]+newValue[5]);
+
+  if (isNaN(red) || isNaN(grn) || isNaN(blu)) {
+    clearResults();
+  } else {
+    $("#results").show();
+
+    $("#swift-code").html("UIColor(red: "+red+", green: "+grn+", blue: "+blu+", alpha: 1) // #"+newValue);
+    $("#objective-c-code").html("[UIColor colorWithRed:"+red+" green:"+grn+" blue:"+blu+" alpha:1]; // #"+newValue);
+
+    $(".color-preview.active").css("border-color", "black");
+    $(".color-preview.active").css("background-color", "#"+newValue);
+  }
+}
+
+function validateValue(value) {
+  if ((value.length == 7 || value.length == 4) && value[0] == '#') {
+    return true;
+  }
+
+  if ((value.length == 6 || value.length == 3) && value[0] != '#') {
+    return true;
+  }
+
+  return false;
 }
 
 function clearResults() {
