4277a48c2caf55437f4cf914c07b14cc4bfdeb5f
Eugene Belinski <ebelinski@protonmail.com>
2021-02-07 14:44:48 -0600

    Allow for hex codes with alpha.

--- js/uihex.js
+++ js/uihex.js
@@ -11,20 +11,27 @@ $(document).ready(function() {
 });
 
 function processValue(value) {
-  if (!validateValueLength(value)) {
+  const hexWithoutHash = (value[0] == '#') ? value.substring(1) : value;
+  
+  if (!validateHexLength(hexWithoutHash)) {
     clearResults();
     return;
   }
-
-  const hexWithoutHash = (value[0] == '#') ? value.substring(1) : value;
+  
   const hexLowercased = hexWithoutHash.toLowerCase();
-  const hexFullLength = fullLengthValue(hexLowercased);
+  const hexFullLength = fullLengthValueForAnalyzing(hexLowercased);
+  const hexFullLengthForDisplay = hexLowercased;
 
   const red = getColorFraction(hexFullLength[0]+hexFullLength[1]);
   const grn = getColorFraction(hexFullLength[2]+hexFullLength[3]);
   const blu = getColorFraction(hexFullLength[4]+hexFullLength[5]);
+  
+  var alp = getColorFraction(hexFullLength[6]+hexFullLength[7]);
+  if (alp == "255/255") {
+    alp = "1"
+  }
 
-  if (red == null || grn == null || blu == null) {
+  if (red == null || grn == null || blu == null || alp == null) {
     clearResults();
     return;
   }
@@ -33,37 +40,45 @@ function processValue(value) {
   
   // SwiftUI
   
-  $("#color-literal-code").html(`Color(#colorLiteral(red: ${red}, green: ${grn}, blue: ${blu}, alpha: 1)) // #${hexFullLength}`);
+  $("#color-literal-code").html(`Color(#colorLiteral(red: ${red}, green: ${grn}, blue: ${blu}, alpha: ${alp})) // #${hexFullLengthForDisplay}`);
   
-  $("#color-code").html(`Color(red: ${red}, green: ${grn}, blue: ${blu}) // #${hexFullLength}`);
+  if (alp == "1") {
+    $("#color-code").html(`Color(red: ${red}, green: ${grn}, blue: ${blu}) // #${hexFullLengthForDisplay}`);
+  } else {
+    $("#color-code").html(`Color(red: ${red}, green: ${grn}, blue: ${blu}, opacity: ${alp}) // #${hexFullLengthForDisplay}`);
+  }
   
   // UIKit
 
-  $("#uicolor-literal-code").html(`#colorLiteral(red: ${red}, green: ${grn}, blue: ${blu}, alpha: 1) // #${hexFullLength}`);
+  $("#uicolor-literal-code").html(`#colorLiteral(red: ${red}, green: ${grn}, blue: ${blu}, alpha: ${alp}) // #${hexFullLengthForDisplay}`);
   
-  $("#uicolor-code").html(`UIColor(red: ${red}, green: ${grn}, blue: ${blu}, alpha: 1) // #${hexFullLength}`);
+  $("#uicolor-code").html(`UIColor(red: ${red}, green: ${grn}, blue: ${blu}, alpha: ${alp}) // #${hexFullLengthForDisplay}`);
   
-  $("#uicolor-objective-c-code").html(`[UIColor colorWithRed:${red} green:${grn} blue:${blu} alpha:1]; // #${hexFullLength}`);
+  $("#uicolor-objective-c-code").html(`[UIColor colorWithRed:${red} green:${grn} blue:${blu} alpha:${alp}]; // #${hexFullLengthForDisplay}`);
 
   $(".color-preview.active").css("border-color", "black");
-  $(".color-preview.active").css("background-color", "#"+hexFullLength);
+  $(".color-preview.active").css("background-color", "#"+hexFullLengthForDisplay);
 }
 
-function validateValueLength(value) {
-  if ((value.length == 7 || value.length == 4) && value[0] == '#') {
-    return true;
-  }
-
-  if ((value.length == 6 || value.length == 3) && value[0] != '#') {
+function validateHexLength(hex) {
+  if (hex.length == 8 | hex.length == 6 || hex.length == 3) {
     return true;
   }
 
   return false;
 }
 
-function fullLengthValue(value) {
-  if (value.length == 3) {
-    return value[0]+value[0] + value[1]+value[1] + value[2]+value[2];
+// 3 and 6 become 8
+function fullLengthValueForAnalyzing(value) {
+  if (value.length == 8) {
+    return value;
+  } else if (value.length == 6) {
+    return `${value}ff`;
+  } else if (value.length == 3) {
+    const r = value[0];
+    const g = value[1];
+    const b = value[2];
+    return `${r}${r}${g}${g}${b}${b}ff`
   }
 
   return value;
